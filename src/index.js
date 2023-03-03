500f0b1708356f26a11111093945ea3ff1372976
AlejandroCRG <alejandrocrg1@gmail.com>
2023-03-03 18:38:32 +0100

    Organize, style everything with animations.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
 import './style.css';
 
+let weatherdiv = document.getElementById('temp');
+let locationdiv = document.getElementById('name');
+let descriptiondiv = document.getElementById('description');
+let image = document.getElementById('image');
 
 async function getWeather(){
     const location = document.getElementById('location').value;
     const Locationresponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=0f22ba557dc196175d48a0811ec6d7ac`, {mode: 'cors'});
     const searchlocation = await Locationresponse.json();
-    let lat = searchlocation[0].lat
-    let lon = searchlocation[0].lon
+    let lat = searchlocation[0].lat;
+    let lon = searchlocation[0].lon;
+    let locationname = searchlocation[0].name;
 
     const Weatherresponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0f22ba557dc196175d48a0811ec6d7ac`, {mode: 'cors'});
     const searchData = await Weatherresponse.json();
 
-    let averagetemp = Math.floor(searchData.main.temp -273)
-    let feeltemp = Math.floor(searchData.main.feels_like -273)
 
-    let WeatherData = DataObject(averagetemp,feeltemp)
+    let averagetemp = Math.floor(searchData.main.temp -273);
+    let weatherdes = searchData.weather[0].main;
+    let imgID = searchData.weather[0].icon;
 
-    return WeatherData
+    image.src = `https://openweathermap.org/img/wn/${imgID}@2x.png`;
+    weatherdiv.innerText = averagetemp += '°C';
+    locationdiv.innerText = locationname;
+    descriptiondiv.innerText = weatherdes;
 }
 
-// Look how to return your object with the data retrieved from the API
+getWeather()
 
-function DataObject(averagetemp,feeltemp){
-    let obj = { 
-        averagetemp: averagetemp,
-        feeltemp: feeltemp
-    }
-    return obj
+function animateSearch(){
+    let search = document.getElementById('location');
 }
 
-let ans = getWeather()
 
-console.log(WeatherData)
+let searchbutton = document.getElementById('searchbutton')
+searchbutton.addEventListener("click", getWeather)
+searchbutton.addEventListener("click", animateSearch)
